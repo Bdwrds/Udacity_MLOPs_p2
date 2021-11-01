b3eeec92469a4353c57257d5c8dc8dfaa6c534d4
Bdwrds <edwards.benjamin.james@gmail.com>
2021-11-01 14:03:08 +0000

    Completed data check step

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,17 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Checking the size of the dataset - not too small or large
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Check the prices are between min and max price
+    """
+    idx = data['price'].between(min_price, max_price)
+    assert idx.shape[0] == data.shape[0]
+
